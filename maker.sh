8bc46d58077bf4f0b2d8c6adaec23997ae44dbca
Peter Preeper <ppreeper@gmail.com>
2026-05-14 16:31:03 -0600

    repo Thu 14 May 2026 04:31:03 PM MDT

--- maker.sh
+++ maker.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/env sh
-DEB=docs/debian
-
-genrelease() {
-    PD=${PWD}
-    cd "${DEB}" || return
-    mkdir -p "dists/stable/main/binary-amd64"
-    dpkg-scanpackages --arch amd64 pool/main >dists/stable/main/binary-amd64/Packages
-    gzip -fk dists/stable/main/binary-amd64/Packages
-    cat >dists/stable/main/binary-amd64/Release <<EOF
-Component: main
-Architecture: amd64
-EOF
-    cd "${PD}" || return
-}
-
-getoda() {
-    rm -fv "${DEB}"/pool/main/o/oda*deb
-    mkdir -p "${DEB}"/pool/main/o
-    cp -v "${HOME}"/workspace/repos/ppreeper/odaspro/bin/*deb "${DEB}"/pool/main/o/.
-}
-
 genhugo() {
     hugo -d docs
 }
@@ -32,7 +11,7 @@ pushrepo() {
 
 # Display usage instructions
 usage() {
-    echo "Usage: $0 {getoda|genrelease|genindex|genhugo|makerepo|pushrepo|build|all}"
+    echo "Usage: $0 {genhugo|pushrepo|build|all}"
     exit 1
 }
 
@@ -43,36 +22,16 @@ fi
 
 # Use a case statement to call the corresponding function
 case "$1" in
-getoda)
-    getoda
-    ;;
-genrelease)
-    genrelease
-    ;;
-genindex)
-    genindex
-    ;;
 genhugo)
     genhugo
-    ;;
-makerepo)
-    getoda
-    genrelease
-    genindex
     ;;
 pushrepo)
     pushrepo
     ;;
 build)
-    getoda
-    genrelease
-    genindex
     genhugo
     ;;
 all)
-    getoda
-    genrelease
-    # genindex
     genhugo
     pushrepo
     ;;
